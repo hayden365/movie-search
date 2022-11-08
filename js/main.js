6ccea3d71ca3b6fefccc7cceef166d6eeffecc7a
Hayden365 <ysmh100@gmail.com>
2022-11-08 22:07:21 +0900

    main-result javascript

--- js/main.js
+++ js/main.js
@@ -1,40 +1,40 @@
 // https://www.omdbapi.com/?i=tt3896198&apikey=bcafafa5
 
 const movieSearch = document.getElementById('movie-search');
-const posterList = document.getElementById('poster-list');
+const mainList = document.getElementById('main-list');
 const resultGrid = document.getElementById('result');
 
-async function loadMovies(searchTerm) {
-  const URL = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=bcafafa5`;
+movieSearch.addEventListener('input', (e) => {
+  const searchTerm = e.target.value;
+  getMovies(searchTerm);
+});
+
+async function getMovies(searchTerm) {
+  const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=bcafafa5`;
   const res = await fetch(`${URL}`);
   const data = await res.json();
-  // console.log(data.Search);
-  if (data.Response === 'True') displayMovieList(data);
+  console.log(data.Search);
+  if (data.Response == 'True') return displayMovieList(data.Search);
 }
-
-function findMovies() {
-  let searchTerm = movieSearch.value.trim();
-  if (searchTerm.length > 0) {
-    loadMovies(searchTerm);
-  }
-}
+console.log(getMovies('lord of the rings'));
 
 function displayMovieList(movies) {
-  posterList.innerHTML = '';
-  for (let idx = 0; idx < movies.lgnth; idx++) {
+  mainList.innerHTML = '';
+  for (let idx = 0; idx < movies.length; idx++) {
     let movieListItem = document.createElement('div');
-    movieListItem.dataset.id = movies[idx].imdbID;
-    movieListItem.classList.add('search-list-item');
-    if (movies[idx].Poster !== 'N/A') moviePoster = movies[idx].Poster;
-    else moviePoster = './image/image_not_found.png';
-    movieListItem.innerHTML = `
-    <div class="movie-poster">
-    <img src="${moviePoster}" alt="" />
-    </div>
-    <div class="movie-info">
-      <h3>${movies[idx].Title}</h3>
-      <p>${movies[idx].Year}</p>
-    </div>`;
-    posterList.appendChild(movieListItem);
+    movieListItem.dataset.id = movies[idx].Title;
+    movieListItem.innerHTML = `<a href="javascript:void(0)" class="main-list-item__link">
+      <div class="title-poster">
+        <picture class="title-poster__image">
+          <img alt="${movies[idx].Title}" src="${movies[idx].Poster}" loading="eager" class="picture__img" />
+        </picture>
+        <div class="title-poster__info">
+          <h3>${movies[idx].Title}</h3>
+          <span>${movies[idx].Year}</span>
+        </div>
+      </div>
+    </a>`;
+
+    mainList.appendChild(movieListItem);
   }
 }
